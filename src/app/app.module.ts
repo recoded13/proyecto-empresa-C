092fd563e00bbb6d2e817e99398372cce7065711
RMTAJhimbrows <moitico428@gmail.com>
2023-05-16 07:52:01 +0200

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductosComponent } from './productos/productos.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
+const appRoutes: Routes = [
+
+  {path:'', component:HomeComponent},
+  {path: 'proyectos', component:ProductosComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductosComponent,
+    HeaderComponent,
+    NavComponent,
+    LoginComponent,
+    FooterComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
